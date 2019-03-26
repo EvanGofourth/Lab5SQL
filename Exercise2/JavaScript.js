ef94361a360817946f66ccb1c318385f93a98b26
Evan Gofourth <evangofourth99@gmail.com>
2019-03-26 16:21:34 -0500

    Completed Exercise2. PHP query works as intended.

--- Exercise2/JavaScript.js
+++ Exercise2/JavaScript.js
@@ -1,8 +1,9 @@
 
 function validate()
 {
-  if(!document.getElementById("USER_ID"))
+  if(!document.getElementById("USER_ID").value)
   {
+    document.getElementById("feedbackBox").innerHTML = "You may not submit a blank ID.";
     return false;
   }
   else
